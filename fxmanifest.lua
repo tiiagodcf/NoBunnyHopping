d297d156f1b42929e0c9e70d9b1cb7b3b8462606
Bobby <103152798+tiiagodcf@users.noreply.github.com>
2023-08-02 14:42:54 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'A script to prevent bunny hopping by making the player fall after excessive jumps'
+author 'Your Name'
+version '1.0.0'
+
+client_script 'client.lua'
