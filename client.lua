008ccf1b71ba6bf93d76c9fde4f8bf09db8b05d4
Bobby <103152798+tiiagodcf@users.noreply.github.com>
2023-08-02 14:43:22 +0100

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,18 @@
+local NumberJump = 3
+
+Citizen.CreateThread(function()
+  local Jump = 0
+  while true do
+    local ped = PlayerPedId()
+    if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
+      Citizen.Wait(250)
+      Jump = Jump + 1
+      if Jump == NumberJump then
+        SetPedToRagdoll(ped, 5000, 1400, 2)
+        Jump = 0
+      end
+    else 
+      Citizen.Wait(250)          
+    end
+  end
+end)
